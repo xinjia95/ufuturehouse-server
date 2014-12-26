2382f4064c6c4b15e4991cd03db8eb452bf5d0d5
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2014-12-26 18:25:43 +0100

    Formulario de inicio de sesión

--- src/Ufuturelabs/Ufuturehouse/Server/BackendBundle/Controller/DefaultController.php
+++ src/Ufuturelabs/Ufuturehouse/Server/BackendBundle/Controller/DefaultController.php
@@ -13,6 +13,12 @@ class DefaultController extends Controller
 
     public function loginAction()
     {
-        return $this->render('BackendBundle:Default:index.html.twig');
+        /** @var \Symfony\Component\Security\Http\Authentication\AuthenticationUtils $helper */
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render('BackendBundle:Default:login.html.twig', array(
+            'last_username' => $helper->getLastUsername(),
+            'error'         => $helper->getLastAuthenticationError(),
+        ));
     }
 }
