da1966a4d2d8f86068b78242c77c0c06813012dd
Álvaro de la Vega Olmedilla <avegao@indra.es>
2015-02-11 13:29:45 +0100

    Template for index frontend

--- src/Ufuturelabs/Ufuturehouse/Server/FrontendBundle/Controller/DefaultController.php
+++ src/Ufuturelabs/Ufuturehouse/Server/FrontendBundle/Controller/DefaultController.php
@@ -6,8 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('FrontendBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('FrontendBundle:Default:index.html.twig');
     }
 }
