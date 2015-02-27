c9bd06ded1d4301677776b9471976bd45cc9102c
Álvaro de la Vega Olmedilla <avegao@indra.es>
2015-02-27 08:29:58 +0100

    Limpieza de código

--- src/Ufuturelabs/Ufuturehouse/Server/BackendBundle/Tests/Controller/LocationsControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Ufuturelabs\Ufuturehouse\Server\BackendBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class LocationsControllerTest extends WebTestCase
-{
-}
