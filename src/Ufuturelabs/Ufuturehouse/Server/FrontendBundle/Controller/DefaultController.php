4eb8b73abe4c78929629a861c1687c39b4dfd3b7
Álvaro de la Vega Olmedilla <avegao@indra.es>
2015-03-03 17:01:37 +0100

    Frontend mostrando datos de inmuebles

--- src/Ufuturelabs/Ufuturehouse/Server/FrontendBundle/Controller/DefaultController.php
+++ src/Ufuturelabs/Ufuturehouse/Server/FrontendBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace Ufuturelabs\Ufuturehouse\Server\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ufuturelabs\Ufuturehouse\Server\HousingBundle\Entity\Housing;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontendBundle:Default:index.html.twig');
+        /** @var \Doctrine\Common\Persistence\ObjectManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        /** @var Housing[] $housings */
+        $housings = $em->getRepository('HousingBundle:Housing')->findLast(12);
+
+        return $this->render('FrontendBundle:Default:index.html.twig', array(
+            'housings' => $housings,
+        ));
     }
 }
