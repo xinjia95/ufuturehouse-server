a8591626a03ec850b23db3603d8ed14eab0abbd5
Álvaro de la Vega Olmedilla <alvarodlvo@gmail.com>
2015-03-08 18:51:14 +0100

    Añadido Bundle de Google Maps [2]

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new Elao\Bundle\FormTranslationBundle\ElaoFormTranslationBundle(),
             new Cocur\Slugify\Bridge\Symfony\CocurSlugifyBundle(),
             new Ivory\GoogleMapBundle\IvoryGoogleMapBundle(),
+            new Widop\HttpAdapterBundle\WidopHttpAdapterBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
